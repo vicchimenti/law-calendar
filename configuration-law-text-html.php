944b08478859a9d0045353e87e90cc3c0f632acb
Vic Chimenti <vicchimenti@users.noreply.github.com>
2019-10-29 11:56:25 -0700

    Create configuration-law-text-html.php

---
+++ configuration-law-text-html.php
@@ -0,0 +1,2 @@
+<t4 type="navigation" name="Events Calendar: create config.php" id="667" /> <!-- generate config.php -->
+<t4 type="navigation" name="Events Calendar: create event-calendar.json" id="666" /> <!-- generate event-calendar.json -->
