3e34bf893c18dd97c30030392ef77f94acfe58ec
Vic Chimenti <vicchimenti@users.noreply.github.com>
2019-11-06 11:36:32 -0800

    fixed typos

--- events-fulltext.php
+++ events-fulltext.php
@@ -1,35 +1,39 @@
 <article>
   <h1><t4 type="content" name="Title" output="normal" modifiers="striptags"  /></h1>
   <p class="meta">
-<?php
-try {
-/* Version 2.2 */
-  $_GET['event_id'] = '<t4 type="meta" meta="content_id" />';
-  $event_type = isset($_GET['event_type']) ? filter_var($_GET['event_type'], FILTER_SANITIZE_STRING) : '';
-
-  //Check if we are in preview
-    if (preg_match("/t4_([0-9]{16,20}+)\.php/Ui", $_SERVER['REQUEST_URI'], $output_array)) {
-        throw new Exception("Sorry, PHP Events Calendar is not available in preview.", 1);
-    }
-
-    //load T4EventsCalendar
-    if (!is_file($_SERVER['DOCUMENT_ROOT']  . $t4_module['config'].'config.php')) {
-        throw new Exception("You need to load the T4EventsCalendar Class dude", 1);
-    }
-
-    //load PHAR file
-    include_once($_SERVER['DOCUMENT_ROOT']  . $t4_module['config'].'config.php');
-    $moduleClass = \Calendar\T4EventsCalendar::module($t4_config, $t4_module);
-
-  /* Start Catch */
-} catch (\Exception $e) {
-    echo '<!--'.$e->getMessage().'-->';
-    ?>
-    <time class="sdate" datetime="<t4 type="content" name="Start Date and Time" date_format="yyyy-d-MM" output="normal" />"><t4 type="content" name="Start Date and Time" date_format="MMMM d, yyyy" output="normal" modifiers=""  /></time><span class="to-date sr-only"> to <time datetime="<t4 type="content" name="End Date and Time" date_format="yyyy-d-MM" output="normal" />"><t4 type="content" name="End Date and Time" date_format="MMMM d, yyyy" output="normal" modifiers=""  /></time></span> - <a href="#location-map"><t4 type="content" name="Venue" output="normal" modifiers="striptags"  /></a>
     <?php
-}
-/* End Catch */
-?>
+    try {
+    /* Version 2.2 */
+      $_GET['event_id'] = '<t4 type="meta" meta="content_id" />';
+      $event_type = isset($_GET['event_type']) ? filter_var($_GET['event_type'], FILTER_SANITIZE_STRING) : '';
+
+      //Check if we are in preview
+        if (preg_match("/t4_([0-9]{16,20}+)\.php/Ui", $_SERVER['REQUEST_URI'], $output_array)) {
+            throw new Exception("Sorry, PHP Events Calendar is not available in preview.", 1);
+        }
+
+        //load T4EventsCalendar
+        if (!is_file($_SERVER['DOCUMENT_ROOT']  . $t4_module['config'].'config.php')) {
+            throw new Exception("You need to load the T4EventsCalendar Class dude", 1);
+        }
+
+        //load PHAR file
+        include_once($_SERVER['DOCUMENT_ROOT']  . $t4_module['config'].'config.php');
+        $moduleClass = \Calendar\T4EventsCalendar::module($t4_config, $t4_module);
+
+      /* Start Catch */
+    } catch (\Exception $e) {
+        echo '<!--'.$e->getMessage().'-->';
+        ?>
+        <time class="sdate" datetime="<t4 type='content' name='Start Date and Time' date_format='yyyy-d-MM' output='normal' />">
+            <t4 type="content" name="Start Date and Time" date_format="MMMM d, yyyy" output="normal" modifiers=""  />
+        </time>
+        <span class="to-date sr-only"> to <time datetime="<t4 type='content' name='End Date and Time' date_format='yyyy-d-MM' output='normal' />"><t4 type="content" name="End Date and Time" date_format="MMMM d, yyyy" output="normal" modifiers=""  /></time>
+        </span> - <a href="#location-map"><t4 type="content" name="Venue" output="normal" modifiers="striptags"  /></a>
+        <?php
+    }
+    /* End Catch */
+    ?>
   </p>
   <p class="intro"><t4 type="content" name="Brief Description" output="normal" modifiers="striptags"  /></p>
 
